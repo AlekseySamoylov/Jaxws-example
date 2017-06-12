b7564a0ee12b5fefb6c6a2d330f369d4b6ea5d8f
AlekseySamoylov <alekseysamoylov89@gmail.com>
2017-06-12 11:00:41 +0300

    Web service completed to work on Wildfly 10.

--- src/main/java/com/alekseysamoylov/jaxws/helloservice/Hello.java
+++ src/main/java/com/alekseysamoylov/jaxws/helloservice/Hello.java
@@ -2,11 +2,13 @@ package com.alekseysamoylov.jaxws.helloservice;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
 /**
  * Hello soap service example
  */
 @WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public class Hello {
     private final String message = "Hello, ";
 
